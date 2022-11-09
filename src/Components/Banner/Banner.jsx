a610979e2adc0dc36a622b46d9b6e2df49577835
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-09 18:03:08 +0300

    created the banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,10 +5,51 @@ const Banner = () => {
   return (
     <div className="banner" id="banner">
       <div className="left">
-        <div className="socials"></div>
-        <div className="content">left</div>
+        <div className="socials">
+          <div className="circle">
+            <a href="https://www.linkedin.com/in/samora-mchuma/" target="blank">
+              <i class="fa-brands fa-linkedin-in"></i>
+            </a>
+          </div>
+          <div className="circle">
+            <a href="https://github.com/smchuma" target="blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </div>
+          <div className="circle">
+            <a href="https://www.instagram.com/samora_mchuma/" target="blank">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <div className="img-content">
+          <div className="img-content-background"></div>
+        </div>
       </div>
-      <div className="right">right</div>
+      <div className="right">
+        <div className="titles">
+          <h2>Hello, I'm</h2>
+          <h1>Samora Mchuma</h1>
+          <h3>Software Developer</h3>
+        </div>
+        <div className="buttons">
+          <button className="btn">Download CV</button>
+          <button className="btn">Contact Me</button>
+        </div>
+      </div>
+      <div class="custom-shape-divider-bottom-1668004751">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
