bf116767f841eff7edab1ba99a7cef3618ec20f6
Samora L Mchuma <smchuma@usiu.ac.ke>
2022-11-09 21:10:57 +0300

    created the about me section

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -4,7 +4,20 @@ import "./AboutMe.scss";
 const AboutMe = () => {
   return (
     <div className="about" id="about">
-      AboutMe
+      <h1>About Me</h1>
+      <div className="about-content">
+        <div className="left">
+          <div className="img">
+            <img
+              src="https://miro.medium.com/max/828/1*IRGHmiGsa16stedQvIaZfw.gif"
+              alt="gif"
+            />
+          </div>
+        </div>
+        <div className="right">
+          <p>Black</p>
+        </div>
+      </div>
     </div>
   );
 };
