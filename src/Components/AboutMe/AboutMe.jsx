5aef4a22be9f82380fe061f7b9437f9ccfa4089c
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-10 19:27:29 +0300

    created the about me section

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -15,7 +15,20 @@ const AboutMe = () => {
           </div>
         </div>
         <div className="right">
-          <p>Black</p>
+          <p>
+            I have a bachelor’s degree in information systems technology from
+            United States International University-Africa and have more than 2
+            years of experience as a front-end developer. I have advanced
+            knowledge of React and Node.js, as well as the ability to work
+            independently and solve problems creatively.
+          </p>
+          <p>
+            I've worked on several projects, including a clothing e-commerce
+            website where users can buy clothes; a cryptocurrency tracker where
+            users can keep track of their cryptocurrencies; and a YouTube clone
+            where users can watch YouTube videos. These projects have helped me
+            improve my programming skills immensely.
+          </p>
         </div>
       </div>
     </div>
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "./Banner.scss";
+import Typical from "react-typical";
 
 const Banner = () => {
   return (
@@ -8,17 +8,17 @@ const Banner = () => {
         <div className="socials">
           <div className="circle">
             <a href="https://www.linkedin.com/in/samora-mchuma/" target="blank">
-              <i class="fa-brands fa-linkedin-in"></i>
+              <i className="fa-brands fa-linkedin-in"></i>
             </a>
           </div>
           <div className="circle">
             <a href="https://github.com/smchuma" target="blank">
-              <i class="fa-brands fa-github"></i>
+              <i className="fa-brands fa-github"></i>
             </a>
           </div>
           <div className="circle">
             <a href="https://www.instagram.com/samora_mchuma/" target="blank">
-              <i class="fa-brands fa-instagram"></i>
+              <i className="fa-brands fa-instagram"></i>
             </a>
           </div>
         </div>
@@ -30,10 +30,24 @@ const Banner = () => {
         <div className="titles">
           <h2>Hello, I'm</h2>
           <h1>Samora Mchuma</h1>
-          <h3>Software Developer</h3>
+          <h3>
+            {" "}
+            <Typical
+              steps={[
+                "Software Developer ",
+                2000,
+                "HTML5/CCS3/JavaScript",
+                3000,
+                "React / Redux",
+                4000,
+              ]}
+              loop={Infinity}
+              wrapper="p"
+            />
+          </h3>
         </div>
         <div className="buttons">
-          <a href="cv.pdf" download="SAMORA MCHUMA CV.pdf">
+          <a href="cv.pdf" download="braCKY.pdf">
             <button className="btn">Download CV</button>
           </a>
           <a href="#contact">
@@ -41,7 +55,7 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div class="custom-shape-divider-bottom-1668009551">
+      <div className="custom-shape-divider-bottom">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +64,7 @@ const Banner = () => {
         >
           <path
             d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-            class="shape-fill"
+            className="shape-fill"
           ></path>
         </svg>
       </div>
