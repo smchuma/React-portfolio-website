6a89e8ebc02f77f73aadf4f82419dc6c4b45ed7b
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-12 12:10:26 +0300

    worked on the service section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
+
 import "./App.scss";
 import {
   Navbar,
@@ -13,19 +15,35 @@ import {
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <div className="app">
-      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <div className="sections">
-        <Banner />
-        <AboutMe />
-        <Portfolio />
-        <Services />
-        <Interests />
-        <Contact />
-      </div>
+      {loading ? (
+        <div className="loader">
+          <HashLoader color={"#b9bdcf"} loading={loading} size={100} />
+        </div>
+      ) : (
+        <>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <div className="sections">
+            <Banner />
+            <AboutMe />
+            <Portfolio />
+            <Services />
+            <Interests />
+            <Contact />
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,9 +2,71 @@ import React from "react";
 import "./Services.scss";
 
 const Services = () => {
+  const items = [
+    {
+      id: 1,
+      title: " HTML/CSS",
+    },
+    {
+      id: 2,
+      title: " JavaScript",
+    },
+    {
+      id: 3,
+      title: " React / Redux",
+    },
+    {
+      id: 4,
+      title: " HTML/CSS",
+    },
+  ];
   return (
     <div className="services" id="Services">
-      services
+      <h1>My Services</h1>
+      <div className="wrapper">
+        <div className="first">
+          <div className="first-content">
+            <img src="assets/web.png" alt="web" />
+            <h2>Development</h2>
+            <ul>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                HTML/CSS
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                JavaScript
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                React / Redux
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                Node / Express
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                Material UI
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                MySQL
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                MongoDB
+              </li>
+              <li>
+                <i class="fa-solid fa-check"></i>
+                Sass
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="second">rr</div>
+        <div className="third">ss</div>
+      </div>
     </div>
   );
 };
