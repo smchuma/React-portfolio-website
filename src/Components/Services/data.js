45565be5095046865f4ef6a7e10a45ea1e34050c
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-12 13:04:21 +0300

    finished the service page

---
+++ src/Components/Services/data.js
@@ -0,0 +1,76 @@
+export const development = [
+  {
+    id: 1,
+    title: " HTML/CSS",
+  },
+  {
+    id: 2,
+    title: " JavaScript",
+  },
+  {
+    id: 3,
+    title: " React / Redux",
+  },
+  {
+    id: 4,
+    title: "Node / Express",
+  },
+  {
+    id: 5,
+    title: " Material UI",
+  },
+  {
+    id: 6,
+    title: "MySQL",
+  },
+  {
+    id: 7,
+    title: "MongoDB",
+  },
+  {
+    id: 8,
+    title: "Sass",
+  },
+];
+
+export const illustration = [
+  {
+    id: 1,
+    title: "Graphic design",
+  },
+  {
+    id: 2,
+    title: "Motion Graphics",
+  },
+  {
+    id: 3,
+    title: "Logo Design",
+  },
+  {
+    id: 4,
+    title: " 2d Animation",
+  },
+];
+
+export const production = [
+  {
+    id: 1,
+    title: "Photography",
+  },
+  {
+    id: 2,
+    title: "Videography",
+  },
+  {
+    id: 3,
+    title: "Video Editing",
+  },
+  {
+    id: 4,
+    title: "Visual Effects",
+  },
+  {
+    id: 5,
+    title: "Music Visualizer",
+  },
+];
