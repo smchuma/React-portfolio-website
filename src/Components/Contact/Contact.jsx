4e77b0c722346639435f034870716707e98f7893
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-17 15:06:40 +0300

    added toastify for the contanct message

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Contact.scss";
 
 const Contact = () => {
@@ -17,8 +19,7 @@ const Contact = () => {
       )
       .then(
         (result) => {
-          console.log(result.text);
-          console.log("message sent");
+          toast(`${result.text}, I will get back to you ASAP!`);
           e.target.reset();
         },
         (error) => {
@@ -35,13 +36,25 @@ const Contact = () => {
       <div className="right">
         <h2>Get in touch</h2>
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" placeholder="Name" />
+          <input type="text" name="user_name" placeholder="Name" required />
 
-          <input type="email" name="user_email" placeholder="Email" />
+          <input type="email" name="user_email" placeholder="Email" required />
 
-          <textarea name="message" placeholder="Message" />
+          <textarea name="message" placeholder="Message" required />
           <input type="submit" value="Send" />
         </form>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
     </div>
   );
