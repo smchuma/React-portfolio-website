74f6db32e5e0e1d2a760684377040924f30306d9
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-18 20:01:59 +0300

    changed portfolio page to use css grid

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -5,18 +5,22 @@ const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
-      <div className="container">
+      <div className="cards">
         {portfolio.map((card) => (
           <div className="card" key={card.id}>
-            <img src={card.img} alt={card.img} className="card-image" />
-            <h3>{card.title}</h3>
-            <div className="buttons">
-              <a href={card.github} target="blank">
-                <button>GITHUB</button>
-              </a>
-              <a href={card.demo} target="blank">
-                <button>DEMO LINK</button>
-              </a>
+            <div className="container">
+              <img src={card.img} alt={card.title} />
+            </div>
+            <div className="details">
+              <h3>{card.title}</h3>
+              <div className="buttons">
+                <a href={card.github} target="blank">
+                  <button>GITHUB</button>
+                </a>
+                <a href={card.demo} target="blank">
+                  <button>DEMO LINK</button>
+                </a>
+              </div>
             </div>
           </div>
         ))}
--- src/Components/Portfolio/data.js
+++ src/Components/Portfolio/data.js
@@ -22,7 +22,7 @@ export const portfolio = [
   },
   {
     id: 24,
-    title: "E-Commerce Shop",
+    title: "Zenku E-Commerce Shop",
     img: "assets/zenku.png",
     github: "https://github.com/smchuma/Clothing-E-commerce-Web-App",
     demo: "https://github.com/smchuma/Clothing-E-commerce-Web-App",
