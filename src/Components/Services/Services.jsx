29399d27db9b529b2236ab807496d4985f832b2d
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-11-18 21:11:06 +0300

    worked on services page

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -11,47 +11,43 @@ const Services = () => {
     <div className="services" id="services">
       <h1>My Services</h1>
       <div className="wrapper">
-        <div className="first">
-          <div className="first-content">
-            <img src="assets/web.png" alt="web" />
-            <h2>Development</h2>
-            <ul>
-              {development.map((item) => (
-                <li key={item.id}>
-                  <i className="fa-solid fa-check"></i>
-                  {item.title}
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className="box">
+          <img src="assets/web.png" alt="web" />
+          <h2>Development</h2>
+          <ul></ul>
+          <ul>
+            {development.map((dev) => (
+              <li key={dev.id}>
+                <i className="fa-solid fa-check"></i>
+                {dev.title}
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="second">
-          <div className="first-content">
-            <img src="assets/illustration.png" alt="web" />
-            <h2>Illustration</h2>
-            <ul>
-              {illustration.map((item) => (
-                <li key={item.id}>
-                  <i className="fa-solid fa-check"></i>
-                  {item.title}
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className="box">
+          <img src="assets/illustration.png" alt="web" />
+          <h2>Illustration</h2>
+          <ul></ul>
+          <ul>
+            {illustration.map((ill) => (
+              <li key={ill.id}>
+                <i className="fa-solid fa-check"></i>
+                {ill.title}
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="third">
-          <div className="first-content">
-            <img src="assets/movie.png" alt="web" />
-            <h2>Production</h2>
-            <ul>
-              {production.map((item) => (
-                <li key={item.id}>
-                  <i className="fa-solid fa-check"></i>
-                  {item.title}
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className="box">
+          <img src="assets/movie.png" alt="web" />
+          <h2>Production</h2>
+          <ul>
+            {production.map((pro) => (
+              <li key={pro.id}>
+                <i className="fa-solid fa-check"></i>
+                {pro.title}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
